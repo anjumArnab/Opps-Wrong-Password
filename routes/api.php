3150fc4eab9803e9ade10103aca7598b37a32e15
anjumArnab <anjumarnab050@gmail.com>
2026-05-26 21:12:16 +0600

    route for create account and login

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('v1')->group(function () {
+    Route::post('create-account', [AuthController::class, 'createAccount']);
+    Route::post('login', [AuthController::class, 'login']);
+});
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
